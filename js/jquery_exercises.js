12068b55674e60ffa9d363c9cfdacc31cae58d67
Sophia <sophia.sudiacal@gmail.com>
2020-05-12 15:22:49 -0500

    bonus: iterating through node list

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -7,13 +7,21 @@
         //    DO NOT have multiple instances of the same ID
         //     $('.codeup').css('border','1px solid red');
 
-        $('li').css('font-size','20px');
+        $('li').css({
+            'font-size': '20px',
+            'background-color' : 'yellow'
+        });
 
-        $('h1,p,li').css('background-color','yellow');
+        $('h1,p').css('background-color','yellow');
         // $('p').css('background-color','yellow');
         // $('li').css('background-color','yellow');
 
-        var myHeadings = $('h1').html();
+        var myHeadings = $('h1');
+
+        //BONUS: iterating through jQuery node list
+        for(var i = 0;i < myHeadings.length;i++){
+            alert(myHeadings[i].innerHTML);
+        }
         alert(myHeadings);
 
     });
