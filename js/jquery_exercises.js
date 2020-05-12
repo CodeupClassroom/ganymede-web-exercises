e468f0dffce910d3539980490b847a57c4c15cfb
Sophia <sophia.sudiacal@gmail.com>
2020-05-12 15:15:31 -0500

    jquery exercises walkthrough

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,21 @@
 "use strict";
 
     $(document).ready(function(){
-        alert('The DOM has finished loading!');
+        // var welcomeHeading = $('#book-welcome').html();
+        // alert(welcomeHeading);
+
+        //    DO NOT have multiple instances of the same ID
+        //     $('.codeup').css('border','1px solid red');
+
+        $('li').css('font-size','20px');
+
+        $('h1,p,li').css('background-color','yellow');
+        // $('p').css('background-color','yellow');
+        // $('li').css('background-color','yellow');
+
+        var myHeadings = $('h1').html();
+        alert(myHeadings);
+
     });
 
 //    Using an IIFE in combination with jQuery DOM ready
