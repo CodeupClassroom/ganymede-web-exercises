ee30a85ffbd59795fe457fb37e99a8745c3523b8
Sophia <sophia.sudiacal@gmail.com>
2020-05-26 12:14:12 -0500

    promises walkthrough

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,35 @@
 "use strict";
 
+// make a function takes in GitHub username
+// returns a promise
+// resolves to date of the most recent commit
+
+function getLastCommitDate(username){
+    return fetch(`https://api.github.com/users/${username}/events`, {
+        headers: {'Authorization': `token ${GITHUB_API_KEY}`}
+    }).then(response => response.json())
+        // we want to filter through PushEvents only
+        .then(events => {
+            return events.filter(event => event.type === "PushEvent")[0].created_at;
+        });
+}
+
+getLastCommitDate('sophiakurihara').then(date => {
+    console.log(new Date(date).toDateString());
+});
+
+function wait(seconds){
+    seconds = seconds * 1000;
+    return new Promise(resolve => {
+       setTimeout(() => {
+           if(seconds > 1000){
+               console.log("You'll see this after " + seconds/1000 + " seconds");
+           }else{
+               console.log("You'll see this after " + seconds/1000 + " second");
+           }
+       },seconds)
+    });
+}
+wait(1);
+wait(2);
+wait(3);
